d821c5244b279b96ee23904d3d944e2644eb5a38
Serhii Kotsar <smotzart@yandex.ru>
2019-02-08 18:45:24 +0200

    change text and images

--- gulpfile.js
+++ gulpfile.js
@@ -43,8 +43,8 @@ gulp.task('fileinclude', function() {
       prefix: '@@',
       basepath: '@root',
       context: {
-        "base_url": "", ///schlaflabor
-        "base_path": "/", ///schlaflabor
+        "base_url": "/schlaflabor", ///schlaflabor
+        "base_path": "/schlaflabor/", ///schlaflabor
         "domain_name": "",
         "facebook_link": "",
         "mailto_link": "office@helios-schlaflabor.at",
